33b2465b1dc5c86f43421ebf6e4b83b76ac892d4
Dhrubo <dhruv.karmaker@gmail.com>
2023-02-25 06:21:34 -0800

    changed origin

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var cors = require('cors');
 const app = express()
 // app.set('trust proxy', 1)
 app.use(cors({
-  origin: "*",
+  origin: "https://cashtrackapp.netlify.app",
   credentials: true
 }));
 app.use(express.json())
